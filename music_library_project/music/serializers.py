273e0cfc58a9a878b00a15d4ce46ef103e5650a6
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-08-09 17:46:49 -0500

    Corrected spelling error of instance in the update method

--- music_library_project/music/serializers.py
+++ music_library_project/music/serializers.py
@@ -1,11 +1,10 @@
-from django.db.models.query import InstanceCheckMeta
 from rest_framework import serializers
 from .models import Song
 
 class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
-        fields = ['id', 'title', 'artist', 'album', 'release_date']
+        fields = ['id', 'title', 'artist', 'album', 'release_date', 'likes']
     
     def create(self, validated_data):
         return Song.objects.create(**validated_data)
@@ -15,5 +14,6 @@ class SongSerializer(serializers.ModelSerializer):
         instance.artist = validated_data.get('artist, instance.artist')
         instance.album = validated_data.get('album, instance.album')
         instance.release_date = validated_data('release_date, instance.release_date')
+        instance.likes = validated_data('likes, instance.likes')
         instance.save()
-        return Instance
+        return instance
