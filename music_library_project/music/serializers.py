95398486bc115013158e04ff2f052baaca38788e
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-08-09 12:23:16 -0500

    updated serializer to be able to create and update

--- music_library_project/music/serializers.py
+++ music_library_project/music/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models.query import InstanceCheckMeta
 from rest_framework import serializers
 from .models import song
 
@@ -5,3 +6,15 @@ class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = song
         fields = ['id', 'title', 'artist', 'album', 'release_date']
+    
+    def create(self, validated_data):
+    return song.objects.create(**validated_data)
+
+    def udpate(self, instance, validated_data):
+        instance.title = validated_data.get('title, instance.title')
+        instance.artist = validated_data.get('artist, instance.artist')
+        instance.album = validated_data.get('album, instance.album')
+        instance.release_date = validated_data('release_date, instance.release_date')
+        instance.save()
+        return Instance
+        
